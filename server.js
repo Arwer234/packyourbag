12020499de75062a5cd7e991bd880feb14feed7f
Arwer234 <Arwer234@users.noreply.github.com>
2020-05-16 18:44:18 +0200

    re

--- server.js
+++ server.js
@@ -8,4 +8,4 @@ app.get('/', (req, res) => {
 })
 
 
-app.listen(3000, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(3000, () => console.log(`Example app listening at: ` + 3000))
