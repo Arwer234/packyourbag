6106da2f939f2f1038a3fa08f3dac6e3d9246b86
Arwer234 <Arwer234@users.noreply.github.com>
2020-05-16 18:51:42 +0200

    serv dir

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var http = require('http').createServer(app);
 app.use(express.static("static"))
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + "/index.html")
+    res.sendFile(__dirname + "/static/index.html")
 })
 
 
