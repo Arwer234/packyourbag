e143dbcb9fc395b713f861988a82842caa0be79c
Arwer234 <Arwer234@users.noreply.github.com>
2020-05-16 18:43:33 +0200

    re

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
+var express = require("express")
+var app = express();
+var http = require('http').createServer(app);
+app.use(express.static("static"))
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.sendFile(__dirname + "/index.html")
 })
 
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(3000, () => console.log(`Example app listening at http://localhost:${port}`))
