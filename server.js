713214c1f58f019e39bbe6d71ba2354f523c9925
Arwer234 <Arwer234@users.noreply.github.com>
2020-07-03 23:31:03 +0200

    new cors

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ var cors = require('cors')
 var http = require('http').createServer(app);
 var itemData = require("./static/js/data")
 app.use(express.static(__dirname + '/static'));
-
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/static/index.html")
 })
