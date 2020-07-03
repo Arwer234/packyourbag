a7fb88f27c7c6cf7f537448d99ca66e191fe0ab8
Arwer234 <Arwer234@users.noreply.github.com>
2020-07-03 13:01:19 +0200

    data server

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 var express = require("express")
 var app = express();
 var http = require('http').createServer(app);
+var itemData = require("./static/js/data")
 app.use(express.static(__dirname + '/static'));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/static/index.html")
 })
-
+app.get('/getItemData', (req,res)=>{
+    console.log(itemData)
+})
 
 app.listen(3000, () => console.log(`Example app listening at: ` + 3000))
